01c7818f6e8451bcdecb364e7dd21a919e5e5056
naninalluri143 <naniblsv143@gmail.com>
2023-10-03 12:49:43 +0530

    "completed"

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.getElementById("email-form").addEventListener("submit", async (e) => {
 
   // Add error handling for the fetch request
   try {
-    const response = await fetch("http://localhost:3000/api/v1/contact/book", {
+    const response = await fetch("http://localhost:3000/sendEmail", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
